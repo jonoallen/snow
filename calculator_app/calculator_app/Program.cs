6260eeb7f99572f00306d93da872fdfc32fc95a8
J Allen <jonoallen@gmail.com>
2018-07-24 21:23:59 -0600

    Include main() functionality for version 1

--- calculator_app/calculator_app/Program.cs
+++ calculator_app/calculator_app/Program.cs
@@ -10,6 +10,30 @@ namespace calculator_app
     {
         static void Main(string[] args)
         {
+            var number1 = getNumberFromUser("Please enter the first number");
+            Calculator.EnterNumber(number1);
+
+            var number2 = getNumberFromUser("Please enter the second number");
+            Calculator.EnterNumber(number2);
+
+            var answer = Calculator.Calculate();
+            Console.WriteLine($"The answer is {answer}");
+                        
+            Console.ReadLine();
+        }
+
+        private static int getNumberFromUser(string prompt)
+        {
+            Console.WriteLine(prompt);
+            var response = Console.ReadLine();
+            if(int.TryParse(response, out int number))
+            {
+                Console.WriteLine();
+                return number;
+            }
+
+            Console.WriteLine("That's not a number.  Please try again.");
+            return getNumberFromUser(prompt);
         }
     }
 }
