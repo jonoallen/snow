7e540d1ae73557ea611ee022259c59b9a9b8bbd9
J Allen <jonoallen@gmail.com>
2018-07-24 21:16:59 -0600

    Initial version - step 1

---
+++ calculator_app/calculator_app/CalculationTestSteps.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace calculator_app
+{
+    [Binding]
+    public sealed class CalculationTestSteps
+    {
+        private int actualResult;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            Calculator.EnterNumber(number);
+        }
+
+        [When("I press add")]
+        public void WhenIPressAdd()
+        {
+            actualResult = Calculator.Calculate();            
+        }
+
+        [Then("the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBe(int expectedResult)
+        {
+            actualResult.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ calculator_app/calculator_app/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator_app
+{
+    public static class Calculator
+    {
+        private static List<int> numbers = new List<int>();
+
+        public static void EnterNumber(int number)
+        {
+            numbers.Add(number);
+        }
+
+        public static int Calculate()
+        {
+            var answer = 0;
+
+            foreach(var number in numbers)
+            {
+                answer = answer + number;
+            }
+
+            return answer;
+        }
+    }
+}
